1cdef7ee53dbc2033263db66d705f9660bec2b1a
? <rm131700@gmail.com>
2024-10-24 18:48:04 +0100

    first commit

---
+++ src/main/java/pt/sapiens/sapiensAPI/SapiensApiApplication.java
@@ -0,0 +1,13 @@
+package pt.sapiens.sapiensAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapiensApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapiensApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/sapiens/sapiensAPI/SapiensApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.sapiens.sapiensAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapiensApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
